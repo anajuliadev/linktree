fa9ffd1e948024e7d18e0896976056299a290307
Ana Júlia <anaju.silvaalves@gmail.com>
2023-08-11 22:33:45 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("mode-light")
+  //if (html.classList.contains("mode-light")) {
+  //html.classList.remove("mode-light")
+  //} else{
+  // html.classList.add("mode-light")
+  //}
+
+//TROCA DE IMG
+  //const img = document.querySelector("#profile img")
+  //if (html.classList.contains("mode-light")) {
+   // img.setAttribute("src", "./assets/light-img.jpg")
+  //} else {
+    //img.setAttribute("src", "https://github.com/anajuliadev.png")
+  //}
+}
